fbdd4d50b1c1f7c2df02c7ae8aa5d4a50a7178e3
Dapo <odapo138@gmail.com>
2024-06-23 04:08:13 +0100

    updated home page hero header

---
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix Clone
+//
+//  Created by Oladapo Olatubosun on 23/06/2024.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
